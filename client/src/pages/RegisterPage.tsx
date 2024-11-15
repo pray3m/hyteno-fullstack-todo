554651e951b846007422a0f90fcecc9fb1433d61
pray3m <prem.gtm9@gmail.com>
2024-11-15 11:23:22 +0545

    fix(routing) : add vercel configuration and redirect user to homepage

--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -1,7 +1,18 @@
 import RegisterForm from "@/layouts/auth/RegisterForm";
-import { FC } from "react";
+import { useAuthStore } from "@/store/authStore";
+import { FC, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const RegisterPage: FC = () => {
+  const { accessToken } = useAuthStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (accessToken) {
+      navigate("/");
+    }
+  }, [accessToken, navigate]);
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <RegisterForm />
